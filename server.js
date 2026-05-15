3bacb4c428c8e947baef8e168c62649525e0079c
Cocainexdoppp <mohdfaheem7014@gmail.com>
2026-05-15 12:38:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,1 @@
+const express = require("express"); const http = require("http"); const socketIo = require("socket.io"); const app = express(); const server = http.createServer(app); const io = socketIo(server, { cors: { origin: "*" } }); app.use(express.static("public")); server.listen(3000, () => { console.log("Server running"); });

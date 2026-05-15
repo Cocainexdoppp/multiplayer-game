b9b38f9aad0183b11ecbf336ea0392152ffbea79
Cocainexdoppp <mohdfaheem7014@gmail.com>
2026-05-15 12:58:51 +0530

    final update
    Added player management and movement handling via socket.io.

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-const express = require("express"); const http = require("http"); const socketIo = require("socket.io"); const app = express(); const server = http.createServer(app); const io = socketIo(server, { cors: { origin: "*" } }); app.use(express.static("public")); server.listen(3000, () => { console.log("Server running"); });
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+app.use(express.static("public"));
+
+let players = {};
+
+io.on("connection", (socket) => {
+
+  players[socket.id] = {
+    x: 100,
+    y: 100
+  };
+
+  io.emit("players", players);
+
+  socket.on("move", (data) => {
+    players[socket.id] = data;
+    io.emit("players", players);
+  });
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    io.emit("players", players);
+  });
+
+});
+
+server.listen(3000, () => {
+  console.log("Server running");
+});
